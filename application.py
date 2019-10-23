e57dc63590ba8716aabda9266db2738688a65cf2
山田健太郎 <01yamaken07@gmail.com>
2019-10-23 14:21:36 +0900

    init

--- application.py
+++ application.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template
-from io import BytesIO
+from flask import Flask, render_template, request, redirect, url_for, send_from_directory, session
+import os
 import urllib
 from matplotlib.backends.backend_agg import FigureCanvasAgg
 from matplotlib.figure import Figure
@@ -8,12 +8,31 @@ import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 import matplotlib.cm as cm    
 import pandas as pd
+import csv
+from werkzeug import secure_filename
 
 
 app = Flask(__name__)
 
+UPLOAD_FOLDER = './uploads'
+ALLOWED_EXTENSIONS = set(['csv'])
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['SECRET_KEY'] = os.urandom(24)
+
+def allowed_file(filename):
+    return '.' in filename and \
+        filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+        
+        
+def read_csv():
+    for layer in layers:
+        df=pd.read_csv('csv')
+        lst+'layer' = df['layer'].abs()
+        
+
 @app.route("/")
 def index():
+        
     df=pd.read_csv('300_5000.csv')
     lst1 = df['7'].abs()
     
@@ -59,6 +78,19 @@ def index():
             "index.html", 
             image = 'static/img/anim' + str(FPS) + '.gif'
             )
+@app.route("/form")
+def show_csv():
+    if request.method == 'POST':
+        send_data = request.files['send_data']
+        if send_data and allowed_file(send_data.filename):
+            filename = secure_filename(send_data.filename)
+            send_data.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            f = open('uploads/' + filename, 'r')
+            f_reader = csv.reader(f)
+            result = list(f_reader)
+
+            return render_template('csv.html', result=result)
+
 
 if __name__ == "__main__":
-    app.run(debug=True, port=9994)
+    app.run(debug=True, port=5000)
